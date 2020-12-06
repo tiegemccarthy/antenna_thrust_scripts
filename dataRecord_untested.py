3efd339d60108ba67a7965f767656420ae816ded
Tiege <tiegem@utas.edu.au>
2020-12-07 02:10:33 +1100

    updated dataRecord script to not average at the end of 5 min blocks - allows us to play around with data filtering

--- dataRecord_untested.py
+++ dataRecord_untested.py
@@ -25,6 +25,7 @@ def spectrumGrab():
 
 def main(scantime_file):
     # load in the scantime_file output by the schedule generator
+    print("Loading scan time file.")
     with open(scantime_file, 'r') as f:
         time_data = [float(line.strip()) for line in f]
     scan_times = Time(time_data, format='jd', scale='utc')
@@ -32,24 +33,27 @@ def main(scantime_file):
     x_inc = 1000.0/501.0
     freq_range = np.arange(1100,2100,x_inc)
     # start the recording
+    print("Starting recording loop.")
     spec_list = []
     while True:
         currenttime = Time(datetime.utcnow()) # grab current UTC time
         for i in range(0, len(scan_times)-1): 
             if scan_times[i+1] > currenttime > (scan_times[i] + timedelta(seconds =90)): # check whether current time falls within 90s of the start of any scan and the end of that same scan
                 if not os.path.exists(cwd + '/' + str(scan_times[i]) + '.dat'): # create a data file for this scan if it doesn't already exist
-                    open(cwd + '/' + str(scan_times[i]) + '.dat', 'a').close()
+                    open(cwd + '/' + str(scan_times[i]) + '.txt', 'a').close()
                 spectrum = spectrumGrab() # grab the spectrum at this time
                 spec_list.append(spectrum) # append it to the list containing all spectra for this current scan
-                time.sleep(4.5)
+                time.sleep(0.5)
                 current_scan = i # this is used for writing out the data between scans
                 break    
             elif i == len(scan_times)-2 and len(spec_list) > 0: # if spectrum data exists and the iterator maxes out then we are between scans - here we write out the data to a file
                 spec_array = np.array(spec_list) # covert list of lists to a X by Y array
-                spec_array_avg = spec_array.mean(axis=0) # average values at each frequency together
-                with open(cwd + '/' + str(scan_times[current_scan]) + '.dat', 'a') as f:
-                    for i in range(0,len(spec_array_avg)):
-                        print([freq_range[i], spec_array_avg[i]], file=f) # write out the data
+                np.savetxt(cwd + '/' + str(scan_times[current_scan]) + '.txt', spec_array, header='Scan start time: ' + scan_times[i] + ' jd', delimiter=",")
+                #spec_array_avg = spec_array.mean(axis=0) # average values at each frequency together
+                #with open(cwd + '/' + str(scan_times[current_scan]) + '.dat', 'a') as f:
+                #    for i in range(0,len(spec_array_avg)):
+                #        print([freq_range[i], spec_array_avg[i]], file=f) # write out the data
+                #        print("Averaged spectrum for scan " + str(scan_times[current_scan]) + " written to file.")
                 spec_list = []
             else:
                 continue
